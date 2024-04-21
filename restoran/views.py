df731158e0d9c56bb91f31b17cfbc675cb7bc4b9
Tolikg63 <ttserunyan@inbox.ru>
2024-04-21 11:31:22 +0400

    Front-end is working correctly!

--- restoran/views.py
+++ restoran/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class BaseIndex(View):
+    def get(self, request):
+        return render(request, 'restoran/index.html')
